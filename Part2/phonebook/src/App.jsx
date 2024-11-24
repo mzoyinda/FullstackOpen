42e5c83a15a04136660d3aeeed579bc4249931d1
oyindamola <oyindamoladawodu22@gmail.com>
2024-11-24 16:53:13 +0100

    step 3 completed

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,55 +1,73 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "0909786474"}]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const alreadyExist = persons.some((person) => person.name === newName);
-  
-    if (!newName.trim()) {
-      alert('Please enter a name');
+
+    if (!newName.trim() || !newNumber.trim()) {
+      alert("Please fill all inputs");
       return;
     }
-  
+ 
+    const alreadyExist = persons.some((person) => person.name === newName || person.number === newNumber);
+
     if (alreadyExist) {
       alert(`${newName} is already added to the phonebook`);
       return;
     }
-  
-    setPersons([...persons, { name: newName }]);
-    setNewName(''); 
-  };
-  
 
-  const handleChange = (e) =>{
-    const value = e.currentTarget.value
-    setNewName(value)
-  }
+    setPersons([...persons, { name: newName, number: newNumber }]);
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleChange = (e) => {
+    const {name, value} = e.currentTarget;
+    if (name === "contact") {
+      setNewName(value);
+    } else {
+      setNewNumber(value);
+    }
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input type='text' value={newName} onChange={handleChange} />
+          name:{" "}
+          <input
+            type="text"
+            name="contact"
+            value={newName}
+            onChange={handleChange}
+          />
         </div>
         <div>
-          <button type="submit" >add</button>
+          number:{" "}
+          <input
+            type="text"
+            name="number"
+            value={newNumber}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-     <div>
-      {persons.map((person, index)=>(
-        <p key={index}>{person.name}</p>
-        
-      ))}
-     </div>
+      <div>
+        {persons.map((person, index) => (
+          <p key={index}>{person.name}: {person.number}</p>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
