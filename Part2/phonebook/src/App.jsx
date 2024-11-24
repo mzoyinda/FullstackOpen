d6cf498dfc5017897167df02c51defce122bbd42
oyindamola <oyindamoladawodu22@gmail.com>
2024-11-24 17:11:18 +0100

    step 4 completed

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "0909786474"}]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredPersons = persons.filter(
+    (persons) =>
+      persons.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      persons.number.includes(searchQuery)
+  );
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,8 +24,10 @@ const App = () => {
       alert("Please fill all inputs");
       return;
     }
- 
-    const alreadyExist = persons.some((person) => person.name === newName || person.number === newNumber);
+
+    const alreadyExist = persons.some(
+      (person) => person.name === newName || person.number === newNumber
+    );
 
     if (alreadyExist) {
       alert(`${newName} is already added to the phonebook`);
@@ -26,7 +40,8 @@ const App = () => {
   };
 
   const handleChange = (e) => {
-    const {name, value} = e.currentTarget;
+    const { name, value } = e.currentTarget;
+
     if (name === "contact") {
       setNewName(value);
     } else {
@@ -34,10 +49,26 @@ const App = () => {
     }
   };
 
+  const handleSearchChange = (e) => {
+    const value = e.currentTarget.value;
+    setSearchQuery(value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with:{" "}
+        <input
+          type="text"
+          name="search"
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />{" "}
+      </div>
+      <br />
       <form onSubmit={handleSubmit}>
+        <h2>Add a new</h2>
         <div>
           name:{" "}
           <input
@@ -60,10 +91,13 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+      <br />
       <h2>Numbers</h2>
       <div>
-        {persons.map((person, index) => (
-          <p key={index}>{person.name}: {person.number}</p>
+        {(searchQuery ? filteredPersons : persons).map((person) => (
+          <p key={person.id}>
+            {person.name}: {person.number}
+          </p>
         ))}
       </div>
     </div>
