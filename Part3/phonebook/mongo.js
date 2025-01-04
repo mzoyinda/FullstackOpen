8547a420c8f628d5ea4ff90d4c62b48481cf73e7
oyindamola <oyindamoladawodu22@gmail.com>
2025-01-04 10:46:30 +0100

    exercise 3.12 completed

---
+++ Part3/phonebook/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.3afvp.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const phoneSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Phone = mongoose.model('Phone', phoneSchema)
+
+const phone = new Phone({
+  name,
+  number
+})
+
+if(name && number){
+  phone.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}else{
+  Phone.find({}).then(result => {
+    result.forEach(phone => {
+      console.log(`${phone.name}  ${phone.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
