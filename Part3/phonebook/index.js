125631fec32d4724481dd41876f859c49406680d
oyindamola <oyindamoladawodu22@gmail.com>
2025-01-07 13:01:06 +0100

    exercise 3.15- 3.18 completed

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -46,14 +46,13 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  console.log(Person);
   Person.find({}).then((persons) => {
     response.json(persons);
   });
 });
 
 const processingTime = new Date();
-const totalPeople = persons.length;
+const totalPeople = Person.length;
 
 app.get("/info", (request, response) => {
   response.send(
@@ -61,7 +60,7 @@ app.get("/info", (request, response) => {
   );
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id)
     .then((result) => {
       if (result) {
@@ -70,72 +69,72 @@ app.get("/api/persons/:id", (request, response) => {
         response.status(404).end();
       }
     })
-    .catch(error => next(error))
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
-  const body = request.body;
-
-  if (!body.number || !body.name) {
-    return response.status(404).json({
-      error: "Name or Number cannot be empty",
-    });
-  }
-
-  Person.findOne({ name: body.name }).then((nameExists) => {
-    if (nameExists) {
-      return response.status(400).json({
-        error: "Name must be unique",
-      });
+app.post("/api/persons", (request, response, next) => {
+    const body = request.body;
+  
+    if (!body.number || !body.name) {
+      return response.status(404).json({ error: "Name or Number cannot be empty" });
     }
-
-    const person = new Person({
-      name: body.name,
-      number: body.number,
-    });
-
-    person.save().then((newPerson) => {
-      response.status(201).json(newPerson);
-    });
+  
+    Person.findOne({ name: body.name })
+      .then((existingPerson) => {
+        if (existingPerson) {
+          return Person.findByIdAndUpdate(
+            existingPerson.id,
+            { number: body.number },
+            { new: true }
+          );
+        }
+        const person = new Person({
+          name: body.name,
+          number: body.number,
+        });
+        return person.save();
+      })
+      .then((result) => response.status(201).json(result))
+      .catch((error) => next(error));
   });
+  
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-    Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-});
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
 
-app.put("/api/persons/:id", (request, response) => {
-    const body = request.body
-    const person = {
-        name: body.name,
-        number: body.number,
-      };
-
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
-    .then(result => {
-      response.status(204).end()
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
     })
-    .catch(error => next(error))
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-  app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } 
-    next(error)
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
   }
-  app.use(errorHandler)
+  next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 
